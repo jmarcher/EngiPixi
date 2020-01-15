1821560119c291a1a601d8dbbcc89ad888c8bd72
Joaquín Marcher <joaquin@marcher.com.uy>
2020-01-15 09:52:20 +0100

    Use more descriptive variable names

--- Engine/Map.cpp
+++ Engine/Map.cpp
@@ -38,14 +38,14 @@ Map::~Map()
 
 void Map::load(int toLoad[20][25])
 {
-    for(auto i = 0; i < 20; ++i) {
-        for(auto j = 0; j < 25; ++j) {
-            map[i][j] = toLoad[i][j];
+    for(auto row = 0; row < 20; ++row) {
+        for(auto column = 0; column < 25; ++column) {
+            map[row][column] = toLoad[row][column];
         }
     }
     this->source.x = this->source.y = 0;
     this->source.w = this->source.h = TILE_SIDE_SIZE;
-    this->destination.w = this->destination.h = TILE_SIDE_SIZE * 2 * 2;
+    this->destination.w = this->destination.h = TILE_SIDE_SIZE * 2;
 
     this->destination.x = this->destination.y = 0;
 }
@@ -53,11 +53,11 @@ void Map::load(int toLoad[20][25])
 void Map::draw()
 {
     int type = 0;
-    for(auto i = 0; i < 20; ++i) {
-        for(auto j = 0; j < 25; ++j) {
-            type = map[i][j];
-            this->destination.x = i * TILE_SIDE_SIZE * 2;
-            this->destination.y = j * TILE_SIDE_SIZE * 2;
+    for(auto row = 0; row < 20; ++row) {
+        for(auto colum = 0; colum < 25; ++colum) {
+            type = map[row][colum];
+            this->destination.x = row * TILE_SIDE_SIZE * 2;
+            this->destination.y = colum * TILE_SIDE_SIZE * 2;
             switch(type) {
             case WATER:
                 TextureManager::draw(this->water, this->source, this->destination);
